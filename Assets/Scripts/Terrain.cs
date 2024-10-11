3f9f4eca5128e1f927f9b066c18b0ff79905cf05
kulacs <gergelymatyas420@gmail.com>
2024-10-11 16:21:54 +0200

    terrain displacement

--- Assets/Scripts/Terrain.cs
+++ Assets/Scripts/Terrain.cs
@@ -5,21 +5,77 @@ using UnityEngine;
 public class Terrain : MonoBehaviour
 {
     
-    ComputeShader displacePlane;
+    [SerializeField] ComputeShader displacePlane;
+
+    ComputeBuffer verticesCompute;
+    ComputeBuffer uvsCompute;
+
+    readonly int 
+        verticesId = Shader.PropertyToID("Vertices"),
+        uvsId = Shader.PropertyToID("UVs"),
+        heightMapId = Shader.PropertyToID("_HeightMap"),
+        displaceStrengthId = Shader.PropertyToID("DisplaceStrength");
 
     void Start()
     {
-        var mesh = GetComponent<Mesh>();
+        //displacePlane = Resources.Load<ComputeShader>("DisplacePlane");
+
+        var mesh = GetComponent<MeshFilter>().mesh;
         Vector3[] verts = mesh.vertices;
         Vector2[] uvs = mesh.uv;
 
-        ComputeBuffer vertices = new ComputeBuffer(verts.Length, sizeof(float) * 3);
-        ComputeBuffer uvs = new ComputeBuffer(uvs.Length, sizeof(float) * 2);
+        verticesCompute = new ComputeBuffer(verts.Length, sizeof(float) * 3);
+        uvsCompute = new ComputeBuffer(uvs.Length, sizeof(float) * 2);
+        verticesCompute.SetData(verts);
+        uvsCompute.SetData(uvs);
 
-        
-        
-        
+        displacePlane.SetBuffer(0, verticesId, verticesCompute);
+        displacePlane.SetBuffer(0, uvsId, uvsCompute);
+        displacePlane.SetTexture(0, heightMapId, GetComponent<MeshRenderer>().material.GetTexture("_HeightMap"));
+        displacePlane.SetFloat(displaceStrengthId, GetComponent<MeshRenderer>().material.GetFloat("_DisplaceStrength"));
+
+        displacePlane.Dispatch(0, Mathf.CeilToInt(verts.Length / 128.0f), 1, 1);
+
+        verticesCompute.GetData(verts);
+        verticesCompute.Release();
+        uvsCompute.Release();
+
+        mesh.vertices = verts;
+        mesh.RecalculateNormals();
+        mesh.RecalculateTangents();
+
+        MeshCollider mc = GetComponent<MeshCollider>();
+        mc.sharedMesh = mesh;
+
     }
 
+    private void Update() {
+        var mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] verts = mesh.vertices;
+        Vector2[] uvs = mesh.uv;
+
+        verticesCompute = new ComputeBuffer(verts.Length, sizeof(float) * 3);
+        uvsCompute = new ComputeBuffer(uvs.Length, sizeof(float) * 2);
+        verticesCompute.SetData(verts);
+        uvsCompute.SetData(uvs);
+
+        displacePlane.SetBuffer(0, verticesId, verticesCompute);
+        displacePlane.SetBuffer(0, uvsId, uvsCompute);
+        displacePlane.SetTexture(0, heightMapId, GetComponent<MeshRenderer>().material.GetTexture("_HeightMap"));
+        displacePlane.SetFloat(displaceStrengthId, GetComponent<MeshRenderer>().material.GetFloat("_DisplaceStrength"));
+
+        displacePlane.Dispatch(0, Mathf.CeilToInt(verts.Length / 128.0f), 1, 1);
+
+        verticesCompute.GetData(verts);
+        verticesCompute.Release();
+        uvsCompute.Release();
+
+        mesh.vertices = verts;
+        mesh.RecalculateNormals();
+        mesh.RecalculateTangents();
+
+        MeshCollider mc = GetComponent<MeshCollider>();
+        mc.sharedMesh = mesh;
+    }
     
 }
